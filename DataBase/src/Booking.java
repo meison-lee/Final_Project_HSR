f915c57f22733ca7f966dc31c575d5985866db60
buttercantfly <sml890115@gmail.com>
2021-05-26 17:22:56 +0800

    Create Booking.java
    大致完成normal booking

---
+++ DataBase/src/Booking.java
@@ -0,0 +1,199 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Booking {
+	JSONObject obj = JSONUtils.getJSONObjectFromFile("/timeTable.json");
+	JSONArray jsonArray = obj.getJSONArray("Array");
+	
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HHmm");
+	
+	public String NormalBooking(String UID, String Ddate, String Rdate, // Ddate出發時間, Rdate返程時間
+			String SStation, String DStation, //S始站, D終站
+			int normalT, int concessionT, int studentT, //一般票, 優待票, 大學生票
+			int AorW, boolean BorS) // 走道or靠窗, 商務或標準車廂
+	{
+		//處理方向
+		
+		//去程方向(記去程就好)
+		int Direction;
+		
+		if (Integer.valueOf(SStation) < Integer.valueOf(DStation)) {
+			Direction = 0; //南向
+		}
+		else{
+			Direction = 1; //北向
+		}
+		
+		//處理時間
+		
+		//去程
+		Date Dedate  = null; //Date object
+		String WoDD  = null; //week of day
+		String Dtime = null; //time
+		
+		if(Ddate != null) {
+			try {
+				Dedate = sdf.parse(Ddate);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			
+			WoDD = getWeekofDay(Dedate);
+			
+			Dtime = Ddate.substring(11);
+		}
+		
+		//回程
+		Date Redate  = null; //Date object
+		String WoDR  = null; //week of day
+		String Rtime = null; //time
+		
+		if(Rdate != null) {
+			try {
+				Redate = sdf.parse(Rdate);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			
+			WoDR = getWeekofDay(Redate);
+			
+			Rtime = Rdate.substring(11);
+		}
+		
+
+		//將符合時間的列車編號做成表格
+		ArrayList<String> Davailable = new ArrayList<String>();
+		ArrayList<String> Ravailable = new ArrayList<String>();
+		
+		/*    確認順序為：
+		 * 1. 確認方向 Direction
+		 * 2. 確認星期 DayofWeek
+		 * 3. 確認是否符合路線(有無抵達始站與終站) Stations
+		 * 4. 確認始站出發時間 Date
+		 */
+		
+		for(int i = 0; i < jsonArray.length(); i++) {
+			
+			JSONObject train = jsonArray.getJSONObject(i);
+			JSONObject timetable = train.getJSONObject("GeneralTimetable");
+			
+			//去程
+			if ((timetable.getJSONObject("GeneralTrainInfo").getInt("Direction") == Direction)
+				//確認方向
+				&& (timetable.getJSONObject("ServiceDay").getInt(WoDD) != 1) 
+				//確認星期
+				&& (trainroutehas(train, SStation, DStation)) 
+				//確認路線
+				&& (dparturetime(Dtime, SStation, timetable.getJSONArray("StopTimes")))) 
+				//確認出發時間
+			{
+				Davailable.add(timetable.getJSONObject("GeneralTrainInfo").getString("TrainNo"));
+			}
+			
+			//回程
+			if ((timetable.getJSONObject("GeneralTrainInfo").getInt("Direction") != Direction)
+				//確認方向
+				&& (timetable.getJSONObject("ServiceDay").getInt(WoDR) != 1) 
+				//確認星期
+				&& (trainroutehas(train, SStation, DStation)) 
+				//確認路線
+				&& (dparturetime(Rtime, DStation, timetable.getJSONArray("StopTimes")))) 
+				//確認出發時間
+			{
+				Ravailable.add(timetable.getJSONObject("GeneralTrainInfo").getString("TrainNo"));
+			}
+		}
+		
+		
+		
+		for(int i = 0; i < jsonArray.length(); i++) {
+			System.out.println(jsonArray.get(i));
+		}
+		
+		
+		if ((normalT+concessionT+studentT > 10) || ((Rdate != null)&&(normalT+concessionT+studentT > 5))) {
+			return "失敗，因訂單預定過多車票(每筆最多10張，來回車票獨立計算)";
+		}
+		
+		if (Dedate.after(null) || Redate.after(null)) {
+			return "失敗，因尚未能預約";
+		}
+		
+		/*下面兩個要參考我們怎麼處理資料
+		 * 大致上如下
+		 * 1. 建list儲存班次編號
+		 * 2. 檢查各種條件 (各個票種, AorW, 時間)，將符合條件的班次存入list
+		 * 3. 透過list輸出各班次資訊
+		 */
+		
+		/* 列出該時段符合條件的車次*/
+		/* 失敗，因為該時段車次座位已售罄*/
+		
+		return null;
+	}
+	
+	/**
+	 * @param time 輸入時間
+	 * @param SStation 起站
+	 * @param StopTimes 該列次停站表
+	 * @return 若該列次該站的出站時間 在 輸入時間 後 則回傳true 反之回傳false
+	 */
+	
+	private boolean dparturetime(String time, String SStation, JSONArray StopTimes) {
+		for (int i=0 ; i < StopTimes.length(); i++) {
+			if (StopTimes.getJSONObject(i).getString("StationID") == SStation){
+				String DepartureTime = StopTimes.getJSONObject(i).getString("DepartureTime").replace(":", "");
+				if (Integer.valueOf(DepartureTime) >= Integer.valueOf(time)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @param train 該列次的JSONobject
+	 * @param SStation 始站
+	 * @param DStation 終站
+	 * @return true 若路線正確 false 反之
+	 */
+	
+	private boolean trainroutehas(JSONObject train, String SStation, String DStation) {
+		boolean S = false;
+		boolean D = false;
+		
+		for (int j = 0; j < train.getJSONArray("StopTimes").length(); j++) {
+			String station = train.getJSONArray("StopTimes").getJSONObject(j).getString("StationID");
+			if (station	== SStation) {
+				S = true;
+			}
+			if (station	== DStation) {
+				D = true;
+			}
+		}
+
+		if (S && D) {
+			return true;
+		}
+		else return false;
+	}
+	
+	private String getWeekofDay(Date date) {
+		
+		String[] weekDays = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        
+        int w = cal.get(Calendar.DAY_OF_WEEK) - 1;
+        if (w < 0)
+            w = 0;
+        
+		return weekDays[w];
+	}
+}
