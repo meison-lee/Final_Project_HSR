4074e0c2a199c51dd6cfbb4cc5768759f7ae3395
Meison Lee <84371764+meisonabulala@users.noreply.github.com>
2021-06-05 17:06:13 +0800

    Update testjsonfile.java

--- TestProject/src/testjsonfile.java
+++ TestProject/src/testjsonfile.java
@@ -10,7 +10,37 @@ import org.json.JSONObject;
 
 public class testjsonfile {
 
-	public static void main(String[] args) throws IOException {
+	public  void throwdata() throws IOException {
+		try {
+			FileReader h = new FileReader("Data/booking.json");
+			BufferedReader tr = new BufferedReader(h);
+			BufferedWriter bw = new BufferedWriter(new FileWriter("Data/file.json"));
+			String str = null;
+			String data = "";
+			while ((str = tr.readLine()) != null) {
+				data = data + str+"\n";
+			}
+			
+					
+			JSONArray dataJSON = new JSONArray(data);
+//			System.out.println(dataJSON.length());
+			dataJSON.remove(0);
+			String ws = dataJSON.toString();
+			System.out.println(ws);
+			bw.write(ws);
+			bw.flush();
+			tr.close();
+			bw.close();
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	public void getdata() throws IOException {
 		try {
 			FileReader h = new FileReader("Data/file.json");
 			BufferedReader tr = new BufferedReader(h);
@@ -23,9 +53,8 @@ public class testjsonfile {
 			
 					
 			JSONArray dataJSON = new JSONArray(data);
-			System.out.println(dataJSON.length());
+//			System.out.println(dataJSON.length());
 //			dataJSON.remove(0);
-//			dataJSON.getJSONObject(0).remove("code");
 			String ws = dataJSON.toString();
 			System.out.println(ws);
 			bw.write(ws);
@@ -34,11 +63,16 @@ public class testjsonfile {
 			bw.close();
 			
 			
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+	}
+	public static void main(String[]arg) throws IOException {
+		testjsonfile n = new testjsonfile();
+		n.throwdata();
+		n.getdata();
 	}
 
 }
