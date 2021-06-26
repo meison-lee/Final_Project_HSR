1917f29be68a1bdcf75b6c0c9589d1df4a6fc36a
Meison Lee <84371764+meisonabulala@users.noreply.github.com>
2021-06-27 06:15:07 +0800

    Update windowRefunding.java

--- TestProject/src/windowRefunding.java
+++ TestProject/src/windowRefunding.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 
 public class windowRefunding {
 
-	private JFrame frame;
+	private JFrame refundFrame;
 	private JTextField textField;
 	private JTextField textField1;
 	private JTextField textField2;
@@ -26,7 +26,7 @@ public class windowRefunding {
 			public void run() {
 				try {
 					windowRefunding window = new windowRefunding();
-					window.frame.setVisible(true);
+					window.refundFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -45,41 +45,41 @@ public class windowRefunding {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 540, 332);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		frame.setVisible(true);
+		refundFrame = new JFrame();
+		refundFrame.setBounds(100, 100, 540, 332);
+		refundFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		refundFrame.getContentPane().setLayout(null);
+		refundFrame.setVisible(true);
 		
 		JLabel lblNewLabel = new JLabel("UID         :");
 		lblNewLabel.setFont(new Font("標楷體", Font.BOLD, 20));
 		lblNewLabel.setBounds(83, 76, 155, 39);
-		frame.getContentPane().add(lblNewLabel);
+		refundFrame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblTicketCode = new JLabel("Ticket Code :");
 		lblTicketCode.setFont(new Font("標楷體", Font.BOLD, 20));
 		lblTicketCode.setBounds(83, 127, 155, 39);
-		frame.getContentPane().add(lblTicketCode);
+		refundFrame.getContentPane().add(lblTicketCode);
 		
 		JLabel lblNewLabel_2 = new JLabel("\u522A\u6E1B\u4EBA\u6578    :");
 		lblNewLabel_2.setFont(new Font("標楷體", Font.BOLD, 20));
 		lblNewLabel_2.setBounds(83, 178, 155, 39);
-		frame.getContentPane().add(lblNewLabel_2);
+		refundFrame.getContentPane().add(lblNewLabel_2);
 		
 		textField = new JTextField();
 		textField.setBounds(238, 84, 194, 24);
-		frame.getContentPane().add(textField);
+		refundFrame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		textField1 = new JTextField();
 		textField1.setColumns(10);
 		textField1.setBounds(238, 135, 194, 24);
-		frame.getContentPane().add(textField1);
+		refundFrame.getContentPane().add(textField1);
 		
 		textField2 = new JTextField();
 		textField2.setColumns(10);
 		textField2.setBounds(238, 186, 194, 24);
-		frame.getContentPane().add(textField2);
+		refundFrame.getContentPane().add(textField2);
 		
 		JButton btnNewButton = new JButton("\u9000\u7968");
 		btnNewButton.setBackground(new Color(230,230,230));
@@ -119,25 +119,25 @@ public class windowRefunding {
 			}
 		});
 		btnNewButton.setBounds(413, 247, 101, 36);
-		frame.getContentPane().add(btnNewButton);
+		refundFrame.getContentPane().add(btnNewButton);
 		
 		JLabel lblNewLabel_1 = new JLabel("HSR \u9000\u7968\u7CFB\u7D71");
 		lblNewLabel_1.setForeground(new Color(0, 0, 128));
 		lblNewLabel_1.setFont(new Font("標楷體", Font.BOLD | Font.ITALIC, 30));
 		lblNewLabel_1.setBounds(12, 12, 333, 52);
-		frame.getContentPane().add(lblNewLabel_1);
+		refundFrame.getContentPane().add(lblNewLabel_1);
 		
 		JButton btnNewButton_1 = new JButton("\u53D6\u6D88");
 		btnNewButton_1.setBackground(new Color(230,230,230));
 		btnNewButton_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				frame.setVisible(false);
+				refundFrame.setVisible(false);
 			}
 		});
 		btnNewButton_1.setFont(new Font("新細明體", Font.PLAIN, 20));
 		btnNewButton_1.setBounds(12, 247, 101, 36);
-		frame.getContentPane().add(btnNewButton_1);
+		refundFrame.getContentPane().add(btnNewButton_1);
 	}
 
 }
